2abca66c7c6d9487e649382c32acdd8f4e101b6f
Jan <jan810693@gmail.com>
2025-04-14 00:59:06 +0530

    environment setup

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from io import BytesIO
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+import os
 
 app = Flask(__name__)
 
@@ -218,4 +219,5 @@ def download_pdf():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
